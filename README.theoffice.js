160e1f8c6ec017c96f2f6b5836e65831fda77c13
Adrienne <adrienne.wallace@gmail.com>
2021-03-22 12:31:54 -0400

    theoffice

---
+++ README.theoffice.js
@@ -0,0 +1,29 @@
+# Dunder Mifflin
+
+You will be creating an API to manage employee data at Dunder Mifflin. 
+
+![gif](https://media.giphy.com/media/vzoYZ7Ef9clbO/giphy.gif)
+
+## Requirements
+* The user can CRUD employee data
+
+## Model
+|Employee|
+| - |
+| id: number |
+| name: string |
+| title: string |
+| department: Department  |
+| salary: number |
+| manager: string |
+
+|Department|
+| - |
+| SALES |
+| CORPORATE |
+| ACCOUNTING  |
+| RECEPTION |
+| HUMAN RECOURCES | 
+
+## Sumission
+Create a pull request with your solution
